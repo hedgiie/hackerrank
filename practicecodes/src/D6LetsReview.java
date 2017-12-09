c170e0e7f19f9f79d9d21ede149ed804b451ce21
nel <nelpontejos@yahoo.com>
2017-12-09 11:01:10 -0800

    clean version

--- practicecodes/src/D6LetsReview.java
+++ practicecodes/src/D6LetsReview.java
@@ -10,19 +10,12 @@ public class D6LetsReview {
         Scanner sc = new Scanner(System.in);
         int item = sc.nextInt();
         String[] names = new String[item];
-//        System.out.println(names.length);
-        // for storing string into array as a test data entry
         for(int counter = 1 ; counter <= names.length ; counter ++){
             Scanner scn = new Scanner(System.in);
-//            Strin item = sc.nextLine();
             String test = scn.nextLine();
             names[counter - 1] = test;
-//            System.out.println(counter+" "+test.length());
+
         }
-//        StringTokenizer oddString = new StringTokenizer("");
-//        StringTokenizer evenString = new StringTokenizer("");
-//
-//        StringTokenizer[] namesResult = new StringTokenizer[item];
 
         String oddString = "";
         String evenString = "";
@@ -32,7 +25,7 @@ public class D6LetsReview {
             String sample = names[counter];
 
             for (int cntr = 0; cntr < sample.length(); cntr++) {
-                // this is odd
+
                 if (cntr % 2 != 0) {
                     oddString += sample.charAt(cntr);
 
@@ -40,7 +33,7 @@ public class D6LetsReview {
                     evenString += sample.charAt(cntr);
                 }
             }
-//            namesResult[counter] = oddString+" "+evenString;
+
             namesResult[counter] = evenString+" "+oddString;
             oddString = "";
             evenString = "";
