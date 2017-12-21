3d325d21d6a5b2023368d5de67eb7337ddef3419
nel <nelpontejos@yahoo.com>
2017-12-20 22:08:44 -0800

    copied to comment section

---
+++ practicecodes/src/Day17MoreExceptions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Day17MoreExceptions{
+    public int power(int x, int y) throws Exception{
+        if(x < 0 || y < 0){
+            throw new Exception("n and p should be non-negative");
+        }
+        else{
+            return (int)Math.pow(x,y);
+        }
+    }
+}
