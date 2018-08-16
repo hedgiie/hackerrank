d7ecb44671d2b9a3b69bd32fcde68dc83d9fd1b2
nel <nelpontejos@yahoo.com>
2018-08-15 18:26:38 -0700

    first round

---
+++ practicecodes/src/practice/problemsolving/DynamicArray.java
@@ -0,0 +1,69 @@
+package practice.problemsolving;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class DynamicArray {
+
+    // Complete the dynamicArray function below.
+    static List<Integer> dynamicArray(int n, List<List<Integer>> queries) {
+        return new LinkedList();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nq = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(nq[0]);
+
+        int q = Integer.parseInt(nq[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, q).forEach(i -> {
+            try {
+                queries.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = dynamicArray(n, queries);
+
+//        bufferedWriter.write(
+//                result.stream()
+//                        .map(Object::toString)
+//                        .collect(joining("\n"))
+//                        + "\n"
+//        );
+
+        System.out.println(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+
+        ////////////////
+
+    }
+}
