9632c4cbff17b312e40f2252b2ae92d3d4708775
nel <nelpontejos@yahoo.com>
2018-10-07 12:53:35 -0700

    solb

--- practicecodes/src/practice/problemsolving/InsertaNodeattheTailofaLinkedList.java
+++ practicecodes/src/practice/problemsolving/InsertaNodeattheTailofaLinkedList.java
@@ -54,8 +54,16 @@ public class InsertaNodeattheTailofaLinkedList {
      *
      */
     static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
-
-
+        if (head == null) {
+            head = new SinglyLinkedListNode(data);
+        } else {
+            SinglyLinkedListNode node = head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new SinglyLinkedListNode(data);
+        }
+        return head;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
