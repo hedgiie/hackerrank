fa7a6d3a679d002c9c27b66c98370c5248c76109
hedgiie <nelpontejos@yahoo.com>
2018-11-17 23:16:47 -0800

    ayos na

--- practicecodes/src/practice/problemsolving/Mergetwosortedlinkedlists.java
+++ practicecodes/src/practice/problemsolving/Mergetwosortedlinkedlists.java
@@ -67,7 +67,20 @@ public class Mergetwosortedlinkedlists {
      *
      */
     static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        return new SinglyLinkedListNode(10);
+        if (head1 == null && head2 == null) return null;
+        else if (head1 == null) return head2;
+        else if (head2 == null) return head1;
+
+        if(head1.data <= head2.data)
+            head1.next = mergeLists(head1.next, head2);
+        else {
+            SinglyLinkedListNode temp = head2;
+            head2 = head2.next;
+            temp.next = head1;
+            head1 = temp;
+            head1.next = mergeLists(head1.next, head2);
+        }
+        return head1;
 
     }
 
