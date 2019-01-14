34901960e36f3bb33d49c274d7f6d2c6cb046637
hedgiie <nelpontejos@yahoo.com>
2019-01-13 21:18:59 -0800

    insert node in specific location

--- practicecodes/src/practice/problemsolving/MergeTwoSortedLinkedList.java
+++ practicecodes/src/practice/problemsolving/MergeTwoSortedLinkedList.java
@@ -66,7 +66,20 @@ public class MergeTwoSortedLinkedList {
      *
      */
     static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        return new SinglyLinkedListNode(10);
+        if (head1 == null && head2 == null) return null;
+        else if (head1 == null) return head2;
+        else if (head2 == null) return head1;
+
+        if(head1.data <= head2.data)
+            head1.next = mergeLists(head1.next, head2);
+        else {
+            SinglyLinkedListNode temp = head2;
+            head2 = head2.next;
+            temp.next = head1;
+            head1 = temp;
+            head1.next = mergeLists(head1.next, head2);
+        }
+        return head1;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
